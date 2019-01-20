511a5793d49f585c8e0bce8f5d03a7d4ee5658da
Suzumiya Haruhi <mswdwhu@gmail.com>
2019-01-21 00:30:40 +0800

    initiate project

---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef BEAUTY_IN_CODE_LIBRARY_H
+#define BEAUTY_IN_CODE_LIBRARY_H
+
+void hello();
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by suzumiya on 1/20/19.
+//
+
+#include <iostream>
+#include "library.h"
+
+int main(int args, char **argv){
+    hello();
+    return 0;
+}
