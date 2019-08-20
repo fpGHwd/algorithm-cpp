feb6fefe8a6692c5f4e20ff46127f8448c1914a1
suzumiya <ggwdwhu@gmail.com>
2019-08-20 22:24:47 +0800

    update

--- src/offer/P56_2.cc
+++ src/offer/P56_2.cc
@@ -2,7 +2,139 @@
 // Created by suzumiya on 8/20/19.
 //
 
+#include <exception>
+#include <cstdio>
+#include <cstring>
 #include "P56_2.h"
+
+using namespace std;
 
 // 数组中唯一只出现一次的数字, 其他都出现了三次
 
+// 负数,位运算不论正数和负数
+int FindNumberAppearingOnce(int numbers[], int length)
+{
+    if(numbers == nullptr || length <= 0){
+//        throw new exception("Invalid input.");
+        printf("Invalid input.");
+    }
+
+    int bits[32] = {0};
+//    memset(bits,0,sizeof(bits) * 32);
+    for(int i = 0; i < length; i++){
+        int bitMask = 1;
+        for(int j = 31; j >=0; j--){    // > 0 -> >= 0
+            if(numbers[i] & bitMask){
+                bits[j]++;
+            }
+            bitMask <<= 1;
+        }
+    }
+
+    int result = 0;
+    for(int i = 0; i < 32; i++){
+        result <<= 1;
+        result += bits[i]%3;
+    }
+
+    return result;
+
+}
+
+
+// ====================测试代码====================
+void Test(const char* testName, int numbers[], int length, int expected)
+{
+    int result = FindNumberAppearingOnce(numbers, length);
+    if(result == expected)
+        printf("%s passed.\n", testName);
+    else
+        printf("%s FAILED.\n", testName);
+}
+
+// 所有数字都是正数，唯一的数字是最小的
+void Test1()
+{
+    int numbers[] = { 1, 1, 2, 2, 2, 1, 3 };
+    int expected = 3;
+    Test("Test1", numbers, sizeof(numbers) / sizeof(int), expected);
+}
+
+// 所有数字都是正数，唯一的数字的大小位于中间
+void Test2()
+{
+    int numbers[] = { 4, 3, 3, 2, 2, 2, 3 };
+    int expected = 4;
+    Test("Test2", numbers, sizeof(numbers) / sizeof(int), expected);
+}
+
+// 所有数字都是正数，唯一的数字是最大的
+void Test3()
+{
+    int numbers[] = { 4, 4, 1, 1, 1, 7, 4 };
+    int expected = 7;
+    Test("Test3", numbers, sizeof(numbers) / sizeof(int), expected);
+}
+
+// 唯一的数字是负数
+void Test4()
+{
+    int numbers[] = { -10, 214, 214, 214 };
+    int expected = -10;
+    Test("Test4", numbers, sizeof(numbers) / sizeof(int), expected);
+}
+
+// 除了唯一的数字，其他数字都是负数
+void Test5()
+{
+    int numbers[] = { -209, 3467, -209, -209 };
+    int expected = 3467;
+    Test("Test5", numbers, sizeof(numbers) / sizeof(int), expected);
+}
+
+// 重复的数字有正数也有负数
+void Test6()
+{
+    int numbers[] = { 1024, -1025, 1024, -1025, 1024, -1025, 1023 };
+    int expected = 1023;
+    Test("Test6", numbers, sizeof(numbers) / sizeof(int), expected);
+}
+
+// 所有数字都是负数
+void Test7()
+{
+    int numbers[] = { -1024, -1024, -1024, -1023 };
+    int expected = -1023;
+    Test("Test7", numbers, sizeof(numbers) / sizeof(int), expected);
+}
+
+// 唯一的数字是0
+void Test8()
+{
+    int numbers[] = { -23, 0, 214, -23, 214, -23, 214 };
+    int expected = 0;
+    Test("Test8", numbers, sizeof(numbers) / sizeof(int), expected);
+}
+
+// 除了唯一的数字，其他数字都是0
+void Test9()
+{
+    int numbers[] = { 0, 3467, 0, 0, 0, 0, 0, 0 };
+    int expected = 3467;
+    Test("Test9", numbers, sizeof(numbers) / sizeof(int), expected);
+}
+
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+    Test4();
+    Test5();
+    Test6();
+    Test7();
+    Test8();
+    Test9();
+
+    return 0;
+}
