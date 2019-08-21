80d3ac126ac90cd945d0c41a00301daa2a81db05
suzumiya <ggwdwhu@gmail.com>
2019-08-21 23:09:13 +0800

    update

---
+++ src/offer/P59.cc
@@ -0,0 +1,207 @@
+//
+// Created by suzumiya on 8/21/19.
+//
+
+#include <vector>
+#include <deque>
+#include <cstdio>
+#include "P59.h"
+
+using namespace std;
+
+// 滑动窗口的最大值
+
+/*
+void SlideWindowMaxNum(int data[], int length, int w_size, int result[])
+{
+}*/
+
+//vector<int> SlideWindwosMaxNum(const vector<int> nums, unsigned int w_size)
+vector<int> maxInWindows(const vector<int> nums, unsigned int w_size)
+{
+    vector<int> result;
+
+    if(w_size < 1 || w_size > nums.size()) //   只对size进行分析
+        return result;
+
+    deque<int> index;
+    for(unsigned int i = 0; i < w_size; i++){
+        while(!index.empty() && nums[index.back()] < nums[i])
+            index.pop_back();
+        index.push_back(i); /// push index
+    }
+
+    for(unsigned int i = w_size; i < nums.size(); i++){
+        result.push_back(nums[index.front()]);
+
+        while(!index.empty() && nums[index.back()] <= nums[i])
+            index.pop_back();
+
+        if(!index.empty() &&  index.front() <= (int)(i - w_size))
+            index.pop_front();
+
+        index.push_back(i); /// push index
+    }
+    result.push_back(nums[index.front()]);
+
+    return result;
+}
+
+// ====================测试代码====================
+void Test(const char* testName, const vector<int>& num, unsigned int size, const vector<int>& expected)
+{
+    if(testName != nullptr)
+        printf("%s begins: ", testName);
+
+    vector<int> result = maxInWindows(num, size);
+
+    vector<int>::const_iterator iterResult = result.begin();
+    vector<int>::const_iterator iterExpected = expected.begin();
+    while(iterResult < result.end() && iterExpected < expected.end())
+    {
+        if(*iterResult != *iterExpected)
+            break;
+
+        ++iterResult;
+        ++iterExpected;
+    }
+
+    if(iterResult == result.end() && iterExpected == expected.end())
+        printf("Passed.\n");
+    else
+        printf("FAILED.\n");
+}
+
+void Test1()
+{
+    int num[] = { 2, 3, 4, 2, 6, 2, 5, 1 };
+    vector<int> vecNumbers(num, num + sizeof(num) / sizeof(int));
+
+    int expected[] = { 4, 4, 6, 6, 6, 5 };
+    vector<int> vecExpected(expected, expected + sizeof(expected) / sizeof(int));
+
+    unsigned int size = 3;
+
+    Test("Test1", vecNumbers, size, vecExpected);
+}
+
+void Test2()
+{
+    int num[] = { 1, 3, -1, -3, 5, 3, 6, 7 };
+    vector<int> vecNumbers(num, num + sizeof(num) / sizeof(int));
+
+    int expected[] = { 3, 3, 5, 5, 6, 7 };
+    vector<int> vecExpected(expected, expected + sizeof(expected) / sizeof(int));
+
+    unsigned int size = 3;
+
+    Test("Test2", vecNumbers, size, vecExpected);
+}
+
+// 输入数组单调递增
+void Test3()
+{
+    int num[] = { 1, 3, 5, 7, 9, 11, 13, 15 };
+    vector<int> vecNumbers(num, num + sizeof(num) / sizeof(int));
+
+    int expected[] = { 7, 9, 11, 13, 15 };
+    vector<int> vecExpected(expected, expected + sizeof(expected) / sizeof(int));
+
+    unsigned int size = 4;
+
+    Test("Test3", vecNumbers, size, vecExpected);
+}
+
+// 输入数组单调递减
+void Test4()
+{
+    int num[] = { 16, 14, 12, 10, 8, 6, 4 };
+    vector<int> vecNumbers(num, num + sizeof(num) / sizeof(int));
+
+    int expected[] = { 16, 14, 12 };
+    vector<int> vecExpected(expected, expected + sizeof(expected) / sizeof(int));
+
+    unsigned int size = 5;
+
+    Test("Test4", vecNumbers, size, vecExpected);
+}
+
+// 滑动窗口的大小为1
+void Test5()
+{
+    int num[] = { 10, 14, 12, 11 };
+    vector<int> vecNumbers(num, num + sizeof(num) / sizeof(int));
+
+    int expected[] = { 10, 14, 12, 11 };
+    vector<int> vecExpected(expected, expected + sizeof(expected) / sizeof(int));
+
+    unsigned int size = 1;
+
+    Test("Test5", vecNumbers, size, vecExpected);
+}
+
+// 滑动窗口的大小等于数组的长度
+void Test6()
+{
+    int num[] = { 10, 14, 12, 11 };
+    vector<int> vecNumbers(num, num + sizeof(num) / sizeof(int));
+
+    int expected[] = { 14 };
+    vector<int> vecExpected(expected, expected + sizeof(expected) / sizeof(int));
+
+    unsigned int size = 4;
+
+    Test("Test6", vecNumbers, size, vecExpected);
+}
+
+// 滑动窗口的大小为0
+void Test7()
+{
+    int num[] = { 10, 14, 12, 11 };
+    vector<int> vecNumbers(num, num + sizeof(num) / sizeof(int));
+
+    vector<int> vecExpected;
+
+    unsigned int size = 0;
+
+    Test("Test7", vecNumbers, size, vecExpected);
+}
+
+// 滑动窗口的大小大于输入数组的长度
+void Test8()
+{
+    int num[] = { 10, 14, 12, 11 };
+    vector<int> vecNumbers(num, num + sizeof(num) / sizeof(int));
+
+    vector<int> vecExpected;
+
+    unsigned int size = 5;
+
+    Test("Test8", vecNumbers, size, vecExpected);
+}
+
+// 输入数组为空
+void Test9()
+{
+    vector<int> vecNumbers;
+    vector<int> vecExpected;
+
+    unsigned int size = 5;
+
+    Test("Test9", vecNumbers, size, vecExpected);
+}
+
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+    Test4();
+    Test5();
+    Test6();
+    Test7();
+    Test8();
+    Test9();
+
+    return 0;
+}
---
+++ src/offer/P59.h
@@ -0,0 +1,14 @@
+//
+// Created by suzumiya on 8/21/19.
+//
+
+#ifndef BEAUTY_IN_CODE_P59_H
+#define BEAUTY_IN_CODE_P59_H
+
+
+class P59 {
+
+};
+
+
+#endif //BEAUTY_IN_CODE_P59_H
