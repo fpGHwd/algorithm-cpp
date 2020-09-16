d482d71bf54b5c29f0d5eb39cc37c24a1fc075db
Wang Ding <ggwdwhu@gmail.com>
2020-09-16 11:27:58 +0800

    update directory

--- src/leetcode/list.cpp
+++ src/leetcode/list.cpp
@@ -2,25 +2,27 @@
 // Created by suzumiya on 4/15/19.
 //
 
-#include "../../include/list.h"
+#include "list.h"
 
-#include <vector>
 #include <cstdlib>
+#include <vector>
 
 using namespace std;
 
-ListNode* makeList(vector<int>& numList){
-    ListNode *root =nullptr, *w = root;
-    if(numList.size() == 0)
-        return root;
-    for(auto it= numList.begin(); it!= numList.end(); it++){
-        ListNode* node = (ListNode*)malloc(sizeof(struct ListNode));
-        node->val = *it;
-        if(w == nullptr){w=node;root=w;}
-        else {
-            w->next = node;
-            w=w->next;
-        }
-    }
+ListNode *makeList(vector<int> &numList) {
+  ListNode *root = nullptr, *w = root;
+  if (numList.size() == 0)
     return root;
+  for (auto it = numList.begin(); it != numList.end(); it++) {
+    ListNode *node = (ListNode *)malloc(sizeof(struct ListNode));
+    node->val = *it;
+    if (w == nullptr) {
+      w = node;
+      root = w;
+    } else {
+      w->next = node;
+      w = w->next;
+    }
+  }
+  return root;
 }
